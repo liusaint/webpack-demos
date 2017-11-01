94b36248454565deb336f5bb1831d81d216bd7db
Runner <841766635@qq.com>
2017-11-02 00:52:05 +0800

    cssloader

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
 	output:{
 		filename:'index.js',
 		path:path.resolve(__dirname, 'dist')
+	},
+	//loaders。
+	module:{
+		rules:[
+		{
+			test:/\.css/,//正则表达式匹配
+			use:['style-loader','css-loader']//用于处理匹配到的文件的模块。
+		}]
 
+		
 	}
 }
